d2e416b72cc94e13c12d7ec125f10b49de2a30fa
work-ham <ilhamahmadre@gmail.com>
2023-05-31 10:18:27 +0700

    Add API Documnetation with swagger UI

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 dotenv.config();
 
 const authRoutes = require('./routes/authRoutes');
@@ -14,6 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use('/auth', authRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Start server
 const PORT = process.env.PORT || 8080;
