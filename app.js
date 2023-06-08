1e26327a22d69a8413be02fc957f2bdb0a8ebc5c
work-ham <ilhamahmadre@gmail.com>
2023-06-08 20:22:37 +0700

    updated app.js

--- app.js
+++ app.js
@@ -18,7 +18,21 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use('/auth', authRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+// HTML Endpoint for API Documentation
+app.get('/', (req, res) => {
+    const html = `
+      <html>
+        <head>
+          <title>RSS REST API</title>
+        </head>
+        <body>
+          <h1>RSS REST API</h1>
+          <p>View the <a href="/api-docs">API documentation</a>.</p>
+        </body>
+      </html>
+    `;
+    res.send(html);
+  });
 // Start server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
